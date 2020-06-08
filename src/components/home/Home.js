8349bb3c0ea02f0483fc1ad353fa6914fe2210f1
Nina Wiik <ninawiik1@outlook.com>
2020-06-08 14:19:28 +0200

    home
    fixed homepg

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -23,7 +23,8 @@ function Home() {
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore .
+              reprehenderit in voluptate velit esse cillum dolore. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit.
             </p>
             <h6>find your accommodation</h6>
             <p>
