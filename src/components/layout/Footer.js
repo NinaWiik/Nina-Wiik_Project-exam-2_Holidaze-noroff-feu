e18b7e7c12610e56ae11710c5974f92d5fd85d2d
Nina Wiik <ninawiik1@outlook.com>
2020-06-11 11:09:14 +0200

    Changed SoMe icons
    changed icons with react-icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,45 +5,34 @@ import Row from "react-bootstrap/Row";
 import Insta from "../../assets/socialmedia/insta.png";
 import Facebook from "../../assets/socialmedia/fb.png";
 import Twitter from "../../assets/socialmedia/twitter.png";
+import { FaTwitter, FaFacebookSquare, FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
     <div>
-      <Container className="container__footer" fluid>
+      <Container className="footer__container" fluid>
         <Row>
-          <Col className="col__footer--left">
+          <Col className="footer__col--left">
             <h5>Our Address</h5>
-            <p className="paragraph__footer--left">Dreggsallmenningen 1,</p>
-            <p className="paragraph__footer--left">5003 Bergen</p>
-            <p className="paragraph__footer--left">Tel: +47 123 45 678</p>
+            <p className="footer__paragraph--left">Dreggsallmenningen 1,</p>
+            <p className="footer__paragraph--left">5003 Bergen</p>
+            <p className="footer__paragraph--left">Tel: +47 123 45 678</p>
           </Col>
-          <Col className="col__footer--middle">
-            <p className="paragraph__footer--middle">
+          <Col className="footer__col--middle">
+            <p className="footer__paragraph--middle">
               Copyright &copy; {new Date().getFullYear()} - HOLIDAZE
             </p>
           </Col>
-          <Col className="col__footer--right">
+          <Col className="footer__col--right">
             <h5>Follow us </h5>
-            <a href="https://instagram.com" className="anchor__footer">
-              <img
-                src={Insta}
-                className="image__footer--some"
-                alt="instagram social media icon"
-              ></img>
+            <a href="https://instagram.com">
+              <FaInstagram className="footer__anchor" />
             </a>
-            <a href="https://facebook.com" className="anchor__footer">
-              <img
-                src={Facebook}
-                className="image__footer--some"
-                alt="facebook social media icon"
-              ></img>
+            <a href="https://facebook.com">
+              <FaFacebookSquare className="footer__anchor" />
             </a>
-            <a href="https://twitter.com" className="anchor__footer">
-              <img
-                src={Twitter}
-                className="image__footer--some"
-                alt="twitter social media icon"
-              ></img>
+            <a href="https://twitter.com">
+              <FaTwitter className="footer__anchor" />
             </a>
           </Col>
         </Row>
